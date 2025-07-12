c3a165e3d92bd2babfcd2f5deca9d6e37ead1e34
Aditya-yadav <rihayadav761@gmail.com>
2025-07-12 11:09:54 +0530

    deploye failed

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import newsletterRoutes from "./routes/newsletter.route.js";
 import transactionRoutes from './routes/transaction.route.js';
-import path from "path";
+
 const app=express();
 dotenv.config();
 
@@ -24,17 +24,11 @@ app.listen(3000, ()=>{
     console.log("Server is running on port 3000!");
 })
 
-const __dirname = path.resolve();
+
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/newsletter',newsletterRoutes);
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
-
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
