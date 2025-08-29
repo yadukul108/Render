27d13fe75e35bcc4b1791fd384034cfa65e13b64
Aditya-yadav <rihayadav761@gmail.com>
2025-08-29 23:15:53 +0530

    restored target

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
     proxy: {
       '/api': {
-        target: 'https://allegro-backend.onrender.com',
+        target: 'http://localhost:3000',
         secure: false,
       },
     },
